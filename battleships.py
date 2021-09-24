693b419645b0600543ce5617dc097bd95bc79617
THEMAUVEMARAUDER <91062154+THEMAUVEMARAUDER@users.noreply.github.com>
2021-09-24 12:17:25 +0100

    basic 2 player with check for win conditions

--- battleships.py
+++ battleships.py
@@ -1,3 +1,6 @@
+#imports
+import random
+
 
 # functions
 def new_grid(x, y):
@@ -13,47 +16,51 @@ def print_grid(grid):
     line = "  "
     for n in range(len(grid[0])):
         line += f"     {n}    "
-    print(line)
+    print("\n", line)
 
     for n in range(len(grid)):
         print(n, grid[n])
+    print("\n")
 
 def shoot(player, ship_grid, shot_grid):
+    print(f"\n{player}'s firing chart:")
     print_grid(shot_grid)
     print(f"{player}, enter coordinates for your shot ! (x then y separated by a space)")
     response = input("x y > ")
     shot = response.split()
-    if ship_grid[int(shot[0])][int(shot[1])] != ["    "]:
-        print("HIT!")
-        shot_grid[int(shot[0])][int(shot[1])] = ["HIT!"]
-    else:
+    shot_x = shot[0]
+    shot_y = shot[1]
+    if ship_grid[int(shot_y)][int(shot_x)] == ["    "]:
         print("MISS")
-        shot_grid[int(shot[0])][int(shot[1])] = ["MISS"]
+        shot_grid[int(shot_y)][int(shot_x)] = ["MISS"]
+    else:
+        print("HIT!")
+        shot_grid[int(shot_y)][int(shot_x)] = ["HIT!"]
+        ship_grid[int(shot_y)][int(shot_x)] = ["HIT!"]
+    print(f"\n{player}'s firing chart:")
     print_grid(shot_grid)
 
-def ai_shoot(ship_grid, shot_grid):
-    # set a global variable to record hit/miss and counter to search adjacent to hit
-    # check hit/miss to govern aiming behaviour
-    # module to search around hits, updating coounter and hit/miss
-    # 2 random numbers from a range of the grid length for random shots
+def win_check(player, ship_grid):
+    counter = 0
+    for n in range(len(ship_grid)):
+        for m in range(len(ship_grid)):
+            if ship_grid[n][m] == ["    "] or ship_grid[n][m] == ["HIT!"]:
+                counter += 1
 
-    shot = response.split()
-    if ship_grid[int(shot[0])][int(shot[1])] != ["    "]:
-        print("HIT!")
-        shot_grid[int(shot[0])][int(shot[1])] = ["HIT!"]
-    else:
-        print("MISS")
-        shot_grid[int(shot[0])][int(shot[1])] = ["MISS"]
-    print_grid(shot_grid)
-
-
-
+    if counter >= (len(ship_grid) * len(ship_grid)):
+        print(f"Congratulations {player}, you have sunk all the enemy vessels !")
+        exit()
 
 def game(play):
+    print("Name of first player ?")
+    player1 = input("> ")
+    print("Name of second player ?")
+    player2 = input("> ")
     while play:
-        shoot("player1", p2_ship_grid, p1_shot_grid)
-        ai_shoot(p1_ship_grid, p2_shot_grid)
-        play = False
+        shoot(player1, p2_ship_grid, p1_shot_grid)
+        win_check(player1, p2_ship_grid)
+        shoot(player2, p1_ship_grid, p2_shot_grid)
+        win_check(player2, p1_ship_grid)
 
 def main(play):
     while play:
@@ -69,20 +76,32 @@ def main(play):
 
 
 # tests
-p1_ship_grid = new_grid(5, 5)
+# p1_ship_grid = new_grid(5, 5)
 p1_shot_grid = new_grid(5, 5)
-#p2_ship_grid = new_grid(10, 10)
+# p2_ship_grid = new_grid(10, 10)
 p2_shot_grid = new_grid(5, 5)
-#print(p1_ship_grid)
-#print_grid(p1_ship_grid)
+# print(p1_ship_grid)
+# print_grid(p1_ship_grid)
 
-p2_ship_grid = [[["Ca-1"],["Ca-2"],["Ca-3"],["Ca-4"],["Ca-5"]],
-                [["Ba-1"],["Ba-2"],["Ba-3"],["Ba-4"],["    "]],
-                [["De-1"],["De-2"],["De-3"],["    "],["    "]],
-                [["Su-1"],["Su-2"],["Su-3"],["    "],["    "]],
+# p2_ship_grid = [[["Ca-1"],["Ca-2"],["Ca-3"],["Ca-4"],["Ca-5"]],
+#                 [["Ba-1"],["Ba-2"],["Ba-3"],["Ba-4"],["    "]],
+#                 [["De-1"],["De-2"],["De-3"],["    "],["    "]],
+#                 [["Su-1"],["Su-2"],["Su-3"],["    "],["    "]],
+#                 [["Pa-1"],["Pa-2"],["    "],["    "],["    "]]]
+
+p1_ship_grid = [[["    "],["    "],["    "],["    "],["    "]],
+                [["    "],["    "],["    "],["    "],["    "]],
+                [["    "],["    "],["    "],["    "],["    "]],
+                [["    "],["    "],["    "],["    "],["    "]],
+                [["Pa-1"],["Pa-2"],["    "],["    "],["    "]]]
+
+p2_ship_grid = [[["    "],["    "],["    "],["    "],["    "]],
+                [["    "],["    "],["    "],["    "],["    "]],
+                [["    "],["    "],["    "],["    "],["    "]],
+                [["    "],["    "],["    "],["    "],["    "]],
                 [["Pa-1"],["Pa-2"],["    "],["    "],["    "]]]
 
 
-print_grid(p2_ship_grid)
+# print_grid(p2_ship_grid)
 
 main(True)
